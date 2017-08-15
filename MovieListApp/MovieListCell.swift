a5d868a2ee55c7680292ec4d06e0d43b1812c37d
Hassan Ahmed <hassan.ahmed@arpatech.com>
2017-08-15 16:08:41 +0500

    Added empty implementation of tableview and custom cell

---
+++ MovieListApp/MovieListCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieListCell.swift
+//  MovieListApp
+//
+//  Created by Hassan Ahmed on 15/08/2017.
+//  Copyright © 2017 Hassan Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class MovieListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieListApp/ViewController.swift
+++ MovieListApp/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let CellIdentifier = "MovieListCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set tableview height to be dynamic
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+        // Set estimated row height allow tableview to approximate scroll indicator
+        self.tableView.estimatedRowHeight = 66
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,25 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+/**
+ Extension - Implements UITableViewDataSource and UITableViewDelegate methods
+ */
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 14
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier) as! MovieListCell
+        
+        return cell
+    }
 }
 
